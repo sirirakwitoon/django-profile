814ad57961b20516a289dbd0396c5d7dbb2401bb
NUTSRK <nutsrk@odds.team>
2021-06-10 23:53:12 +0700

    add test should not save subsriber email when email is invalid

--- nutty/myprofile/tests.py
+++ nutty/myprofile/tests.py
@@ -14,23 +14,44 @@ class TestProfile(TestCase):
 
 class TestIndexView(TestCase):
     def test_index_view_should_my_name(self):
+        # Given
         Profile.objects.create(name="Sirirk")
 
+        # When
+        response = self.client.get("/")
         response = self.client.get("/")
 
-        response = self.client.get("/")
+         # Then
         assert response.status_code == 200
 
     def test_index_view_should_save_subscriber_email_when_input_form(self):
+        # Given
 
         Profile.objects.create(name="Sirirak")
 
+        # When
         data = {
-            "email" "nutsrk@odds.team"
-
+            "email":"nutsrk@odds.team"
         }
         response = self.client.post("/", data=data)
 
         # Then
-        a = Subscriberlist.objects.last()
-        assert a.email == "nutsrk@odds.team"
+        subscriber = Subscriberlist.objects.last()
+        assert subscriber.email == "nutsrk@odds.team"
+
+    def test_index_view_should_not_save_subscriber_email_when_email_is_invalid(self):
+        # Given
+        Profile.objects.create(name="Sirirak")
+
+        #When
+        data ={
+            "email":"nutty.com"
+        }
+        self.client.post("/",data={"email":"nutsrk@odds.team"})
+        self.client.post("/",data=data)
+
+        #Then
+        subscriber = Subscriberlist.objects.last()
+        assert subscriber.email != "nutty.com"
+
+    
